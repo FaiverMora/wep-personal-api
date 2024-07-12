4b99ca49f095a6ca00ff5acefbfcdc867708ca2e
ronaldalvareze <ap3717402@gmai.com>
2024-07-11 19:53:02 -0500

    creacion de carpertas backend

---
+++ backend/server/connstants.js
@@ -0,0 +1,14 @@
+const DB_USER = "Mora"
+const DB_PASSWORD ="Nike1985"
+const DB_HOST = "mora.2q9jgni.mongodb.net"
+
+const API_VERSION ="v1"
+const IP_SERVER = "localhost"
+
+module.exports ={
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+}
