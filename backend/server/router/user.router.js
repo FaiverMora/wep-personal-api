8634f4cddbcc99432c2c9bc12b60d88e71440bdc
faivermora <ap3717402@gmai.com>
2024-07-20 18:36:36 -0500

    create-User-multiparty

--- backend/server/router/user.router.js
+++ backend/server/router/user.router.js
@@ -1,11 +1,13 @@
 const express =require("express")
+const multiparty = require ("connect-multiparty")
 const UserController = require("../controllers/user.controllers")
 const md_auth = require("../middlewares/authenticated")
 
+const md_upload = multiparty({ uploadDir: "./uploads/avatar"})
 const api = express.Router()
 
 api.get("/user/me", [md_auth.asureAuth],UserController.getMe);
 api.get("/Users", [md_auth.asureAuth], UserController.getUsers);
-api.get("/user", [md_auth.asureAuth], UserController.createUser);
+api.post("/user", [md_auth.asureAuth,md_upload ], UserController.createUser);
 
 module.exports = api
